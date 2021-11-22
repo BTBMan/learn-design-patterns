0240eb60bf1fce48d09b2884253832c67d8d872a
BTBMan <1498118157@qq.com>
2021-11-22 20:24:09 +0800

    手写bind函数 更正07.js的解释

--- this&call&apply/07.js
+++ this&call&apply/07.js
@@ -3,9 +3,9 @@ const obj = {
   name: 'john',
 };
 
-const fn = function (age) {
-  console.log(this.name, age);
+const fn = function (age, gender) {
+  console.log(this.name, age, gender);
 };
 
-const runFn = fn.bind(obj, 11); // 11的这个参数优先级大于调用函数是的传参
-runFn(12); // 参数会被bind的参数覆盖掉 如果bind没有传参 则使用当前的参数
+const runFn = fn.bind(obj, 11); // 11的这个参数的顺序要前于调用函数时的传参
+runFn(12); // 参数会被合并到bind的参数的后面
