c752ebb4aa479a819d4925f5ee3be01c543ce331
BTBMan <1498118157@qq.com>
2021-11-30 21:10:03 +0800

    函数柯里化思想1

---
+++ closure&HOF/08.js
@@ -0,0 +1,16 @@
+// currying (函数柯里化) 的思想1
+
+// 示例代码
+let monthlyCost = 0;
+
+const cost = function (money) {
+  monthlyCost += money;
+};
+
+cost(100); // 第一天
+cost(100); // 第二天
+// ...
+cost(100); // 第三十天
+
+console.log(monthlyCost);
+// 以上代码 实际上我们不关心每天花了多少钱 只关心月底花了多少钱 所以只在月底计算一次就可以了
