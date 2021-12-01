9e82bc25a3229ddb18a7823503d1a0fc8f7b290b
BTBMan <1498118157@qq.com>
2021-12-01 21:08:13 +0800

    uncurrying

---
+++ closure&HOF/11.js
@@ -0,0 +1,19 @@
+// uncurrying
+Function.prototype.uncurrying = function () {
+  const that = this;
+
+  return function () {
+    // 取到第一个参数 为this的指向 然后把剩余的参数通过Funciton.prototype.call的方法实现push
+    const obj = [].shift.call(arguments);
+
+    return that.apply(obj, arguments);
+  };
+};
+
+const push = [].push.uncurrying();
+
+(function () {
+  push(arguments, 4);
+
+  console.log(arguments); // [1,2,3,4]
+})(1, 2, 3);
